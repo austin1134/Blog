57ba29cc2bf5acd3c4c673f77e27a1c6fec414c9
austin1134 <austin.torres@colorado.edu>
2016-01-26 16:51:19 -0500

    First blog commit

---
+++ Blog/Migrations/Configuration.cs
@@ -0,0 +1,127 @@
+namespace Blog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Blog.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Blog.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Blog.Models.ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole {Name = "Admin"});
+            }
+            if (!context.Roles.Any(r => r.Name == "Moderator"))
+            {
+                roleManager.Create(new IdentityRole {Name = "Moderator"});
+            }
+
+            var uStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(uStore);
+
+            if (userManager.FindByEmail("austin.torres@colorado.edu") != null)
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "austin.torres@colorado.edu",
+                    Email = "austin.torres@colorado.edu",
+                    FirstName = "Austin",
+                    LastName = "Torres",
+
+                }, "austint1");
+            }
+
+            var userId = userManager.FindByEmail("austin.torres@colorado.edu").Id;
+            userManager.AddToRole(userId, "Admin");
+            {
+                userManager.AddToRole(userId, "Admin");
+            }
+
+            //add Bobby to the database
+            if (userManager.FindByEmail("bdavis@coderfoundry.com") == null)
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "bdavis@coderfoundry.com",
+                    Email = "bdavis@coderfoundry.com",
+                    FirstName = "Bobby",
+                    LastName = "Davis"
+                }, "Password-1");
+            }
+
+            userId = userManager.FindByEmail("bdavis@coderfoundry.com").Id;
+            if (!userManager.IsInRole(userId, "Moderator"))
+            {
+                userManager.AddToRole(userId, "Moderator");
+            }
+
+            if (userManager.FindByEmail("ajensen@coderfoundry.com") == null)
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "ajensen@coderfoundry.com",
+                    Email = "ajensen@coderfoundry.com",
+                    FirstName = "Andrew",
+                    LastName = "Jensen"
+                }, "Password-1");
+            }
+
+            userId = userManager.FindByEmail("ajensen@coderfoundry.com").Id;
+            if (!userManager.IsInRole(userId, "Moderator"))
+            {
+                userManager.AddToRole(userId, "Moderator");
+            }
+
+            if (userManager.FindByEmail("rmanglani@coderfoundry.com") == null)
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "rmanglani@coderfoundry.com",
+                    Email = "rmanglani@coderfoundry.com",
+                    FirstName = "Ria",
+                    LastName = "Manglani"
+                }, "Password-1");
+            }
+
+            userId = userManager.FindByEmail("rmanglani@coderfoundry.com").Id;
+            if (!userManager.IsInRole(userId, "Moderator"))
+            {
+                userManager.AddToRole(userId, "Moderator");
+            }
+
+            if (userManager.FindByEmail("tjones@coderfoundry.com") == null)
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "tjones@coderfoundry.com",
+                    Email = "tjones@coderfoundry.com",
+                    FirstName = "TJ",
+                    LastName = "Jones"
+                }, "Password-1");
+            }
+
+            userId = userManager.FindByEmail("tjones@coderfoundry.com").Id;
+            if (!userManager.IsInRole(userId, "Moderator"))
+            {
+                userManager.AddToRole(userId, "Moderator");
+            }
+
+            //add other Coder Foundry personnel to the database (assign a default password, as above)
+            // Andrew Jensen ajensen@coderfoundry.com
+            //Ria Manglani rmanglani@coderfoundry.com
+            //TJ Jones tjones@coderfoundry.com
+
+        }
+    }
+}
---
+++ Blog/Migrations/Models/BlogModels.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using Blog.Models;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            this.Comments = new HashSet<Comment>();
+            this.Categories = new HashSet<Category>();
+
+        }
+
+        public int Id { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Updated { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string MediaUrl { get; set; }
+        public bool Published { get; set; }
+
+        public virtual ICollection<Category>Categories { get; set; }
+        public virtual ICollection<Comment>Comments { get; set; }
+    }
+
+    public class Category
+    {
+        public Category()
+        {
+            this.Posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+
+    public class Comment
+    {
+        public Comment()
+        {
+            this.Comments = new HashSet<Comment>();
+        }
+
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string EditorId { get; set; }
+        public string Body { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Updated { get; set; }
+        public int ParentCommentId { get; set; }
+        public bool MarkForDeletion { get; set; }
+
+        public Post Post { get; set; }
+        public ApplicationUser Author { get; set; }
+        public ApplicationUser Editor { get; set; }
+        public Comment ParentComment { get; set; }
+
+        public virtual ICollection<Comment>Comments { get; set; }
+    }
+}
